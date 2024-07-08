a5b957b92504051120e518e4698ed59945bdf9eb
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-07-09 02:46:21 +0530

    Ok....

--- ANNIEMUSIC/plugins/admins/hitag.py
+++ ANNIEMUSIC/plugins/admins/hitag.py
@@ -272,7 +272,7 @@ async def mentionall(client, message):
         pass
 
 
-@app.on_message(filters.command(["vctag"], prefixes=["/"]))
+@app.on_message(filters.command(["eqtag"], prefixes=["/"]))
 async def mention_allvc(client, message):
     chat_id = message.chat.id
     if message.chat.type == ChatType.PRIVATE:
@@ -317,7 +317,7 @@ async def mention_allvc(client, message):
 
 
 
-@app.on_message(filters.command(["histop", "vcstop"]))
+@app.on_message(filters.command(["histop", "eqstop"]))
 async def cancel_spam(client, message):
     if not message.chat.id in spam_chats:
         return await message.reply("๏ ᴄᴜʀʀᴇɴᴛʟʏ ɪ'ᴍ ɴᴏᴛ ᴛᴀɢɢɪɴɢ ʙᴀʙʏ.")
