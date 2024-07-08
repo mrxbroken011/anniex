3002cc40539ab1a3c37bd280c57da3ec21e62762
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-07-09 04:23:45 +0530

    Ok

--- ANNIEMUSIC/plugins/tools/ping.py
+++ ANNIEMUSIC/plugins/tools/ping.py
@@ -17,7 +17,7 @@ from config import BANNED_USERS
 async def ping_com(client, message: Message, _):
     start = datetime.now()
     response = await message.reply_video(
-        video="https://telegra.ph/file/634addcd0a2828f4e8913.mp4",
+        video="https://telegra.ph/file/4f0a703ae7b10a7049c61.mp4",
         caption=_["ping_1"].format(app.mention),
     )
     pytgping = await JARVIS.ping()
