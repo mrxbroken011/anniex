9104fb71667ff2db83f46c8484345dd9dc2fc3f9
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-07-09 01:40:58 +0530

    Update bot.py

--- ANNIEMUSIC/core/bot.py
+++ ANNIEMUSIC/core/bot.py
@@ -8,9 +8,9 @@ from ..logging import LOGGER
 
 class JARVIS(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Annie is on the way...")
+        LOGGER(__name__).info(f"MUSICBROKN is on the way...")
         super().__init__(
-            name="𝐀𝐍𝐍𝐈𝐄 𝐗 𝐌𝐔𝐒𝐈𝐂",
+            name="MUSICBROKN",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
