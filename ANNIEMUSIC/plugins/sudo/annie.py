726b76e1196e47e5490230b9fb8b83550d379b78
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-07-09 11:35:10 +0530

    ....

--- ANNIEMUSIC/plugins/sudo/annie.py
+++ ANNIEMUSIC/plugins/sudo/annie.py
@@ -55,7 +55,7 @@ channel = ["channel"]
 # ========================================= #
 
 
-@app.on_message(filters.command(["umi", "aby"], prefixes=["Y", "y", "b", "B"]) & admin_filter)
+@app.on_message(filters.command(["roken", "aby"], prefixes=["b", "B"]) & admin_filter)
 async def restriction_app(app :app, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
