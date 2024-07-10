331a08022d1e22de193a4eeb24e94dfd4057cd8f
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-07-10 18:14:56 +0530

    Update tempgpt.py

--- ANNIEMUSIC/plugins/tools/tempgpt.py
+++ ANNIEMUSIC/plugins/tools/tempgpt.py
@@ -5,7 +5,7 @@ from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
 from MukeshAPI import api
 
-@app.on_message(filters.command(["chatgpt", "ai", "ask", "arvis", "iri"], prefixes=[".", "J", "j", "s", "S", "/"]))
+@app.on_message(filters.command(["chatgpt", "ai", "ask", "arvis", "umi"], prefixes=[".", "J", "j", "y", "Y", "/"]))
 async def chat_gpt(bot, message):
     try:
         await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
