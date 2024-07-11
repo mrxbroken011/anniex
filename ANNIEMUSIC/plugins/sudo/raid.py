2888526b8184cec94398f0426679d4ec47b88a0d
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-07-11 16:44:11 +0530

    .

--- ANNIEMUSIC/plugins/sudo/raid.py
+++ ANNIEMUSIC/plugins/sudo/raid.py
@@ -132,32 +132,32 @@ RAIDS = [
 @app.on_message(filters.command("raid", prefixes=".") & SUDOERS)
 def spam_command(client, message):
     try:
-        
         message.delete()
     except pyrogram.errors.exceptions.FloodWait as e:
         print(f"Error deleting message: {e}")
         pass  
 
-    
+    command_args = message.text.split(" ", 2)[1:]
+
+    if len(command_args) < 1:
+        message.reply_text("Incorrect usage.\nUsage: .raid <count> <username> or reply to target message")
+        return
+
+    try:
+        num_times = int(command_args[0])
+    except ValueError:
+        message.reply_text("Invalid number format for count.\nUsage: .raid <count> <username> or reply to target message")
+        return
+
     if message.reply_to_message:
         user_to_tag = message.reply_to_message.from_user.mention()
-        command_args = message.text.split(" ", 1)[1:]
-
-        if len(command_args) == 1:
-            try:
-                num_times = int(command_args[0])
-            except ValueError:
-                message.reply_text("**Invalid number format for amount.**\n**Usage:** .raid <amount>")
-                return
-        else:
-            message.reply_text("**Incorrect usage.**\n**Usage:** .raid <amount>")
-            return
-
-        for _ in range(num_times):
-            
-            text_to_spam = random.choice(RAIDS)
-            
-            message.reply_text(f"{user_to_tag} **{text_to_spam}**")
-            time.sleep(0.5)  
+    elif len(command_args) == 2:
+        user_to_tag = command_args[1]
     else:
-        message.reply_text("**Reply to a message and use the .raid command to spam.**\n**Usage:** .raid <amount>")
+        message.reply_text("Incorrect usage.\nUsage: .raid <count> <username> or reply to target message")
+        return
+
+    for _ in range(num_times):
+        text_to_spam = random.choice(RAIDS)
+        message.reply_text(f"{user_to_tag} {text_to_spam}")
+        time.sleep(0.5)
