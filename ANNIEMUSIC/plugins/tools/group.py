168c7097db01ba95cfa97a651ed732af40e4bca2
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-07-18 19:34:49 +0530

    Update group.py

--- ANNIEMUSIC/plugins/tools/group.py
+++ ANNIEMUSIC/plugins/tools/group.py
@@ -5,11 +5,11 @@ from config import OWNER_ID
 # vc on
 @app.on_message(filters.video_chat_started)
 async def brah(_, msg):
-       await msg.reply("ᴠᴏɪᴄᴇ ᴄʜᴀᴛ sᴛᴀʀᴛᴇᴅ")
+       await msg.reply("🎙️**ᴠᴏɪᴄᴇ ᴄʜᴀᴛ sᴛᴀʀᴛᴇᴅ ʟᴇᴛ's ᴘʟᴀʏ sᴏɴɢs**")
 # vc off
 @app.on_message(filters.video_chat_ended)
 async def brah2(_, msg):
-       await msg.reply("**ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴇɴᴅᴇᴅ**")
+       await msg.reply("🔇**ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴇɴᴅᴇᴅ ᴛʜᴀɴᴋs ғᴏʀ ᴊᴏɪɴɪɴɢ**")
 
 # invite members on vc
 @app.on_message(filters.video_chat_members_invited)
