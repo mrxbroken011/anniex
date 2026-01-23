ab935d57fcc629d4f4f267051941c87b79b91cd6
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2026-01-23 15:27:49 +0530

    Add files via upload

--- ANNIEMUSIC/core/bot.py
+++ ANNIEMUSIC/core/bot.py
@@ -1,16 +1,13 @@
 from pyrogram import Client, errors
 from pyrogram.enums import ChatMemberStatus, ParseMode
-
 import config
-
 from ..logging import LOGGER
-
 
 class JARVIS(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"MUSICBROKN is on the way...")
+        LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            name="MUSICBROKN",
+            name="ANNIEMUSIC",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
